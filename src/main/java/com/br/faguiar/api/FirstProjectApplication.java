947245296ee913e26bc39cb9da36f5242937c0f9
FranciscoAguiar <fdama.cid20@uea.edu.br>
2022-05-26 12:02:12 -0400

    test

--- src/main/java/com/br/faguiar/api/FirstProjectApplication.java
+++ src/main/java/com/br/faguiar/api/FirstProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstProjectApplication {
 /**
- * 
+ * Test
  * @param args
  */
 	public static void main(String[] args) {
