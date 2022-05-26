0e4ca1c233c0506b54313c8d9a4a1ce5497923ac
FranciscoAguiar <fdama.cid20@uea.edu.br>
2022-05-26 11:56:47 -0400

    documentation

--- src/main/java/com/br/faguiar/api/FirstProjectApplication.java
+++ src/main/java/com/br/faguiar/api/FirstProjectApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstProjectApplication {
-
+/**
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectApplication.class, args);
 	}
